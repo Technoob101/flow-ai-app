8813a6524f81ec92d505314d121c14894dc4bc36
Technoob101 <supakitprin@gmail.com>
2024-08-01 01:14:32 +0700

    styling search bar

--- client/src/components/UserPrompt.jsx
+++ client/src/components/UserPrompt.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 import './UserPrompt.css'
 
 function UserPrompt() {
+  
+  useEffect(() => {
+    const textarea = document.querySelector('textarea');
+    if (textarea) {
+      const adjustHeight = e => {
+        textarea.style.height = "auto";
+        let scHeight = e.target.scrollHeight;
+        textarea.style.height = `${scHeight}px`;
+      };
+
+      textarea.addEventListener("keyup", adjustHeight);
+
+      // Cleanup event listener on component unmount
+      return () => {
+        textarea.removeEventListener("keyup", adjustHeight);
+      };
+    }
+  }, []);
+  
   return (
     <div className='userprompt'>
-        <input 
-            type="text"
-            placeholder='ex: Where is Messi...?' 
-        />
+        <div className='inputwrap'>
+          <textarea rows='1' placeholder='ex: latest llm research..'></textarea>
+          <button className='sendprompt'>=&gt;</button>
+        </div>
     </div>
   )
 }
